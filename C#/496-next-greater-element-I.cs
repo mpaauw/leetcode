b161a9e201fbca9e3033ad15906532399c16affb
Matt <matt.paauw@gmail.com>
2017-04-03 20:49:01 -0700

    Added algorithm 496.

---
+++ C#/496-next-greater-element-I.cs
@@ -0,0 +1,52 @@
+public class Solution
+{
+    public int[] NextGreaterElement(int[] findNums, int[] nums)
+    {
+        int max = -1;
+        if(nums.Length > 0) {
+            int max = nums[0];
+        }
+        
+        for (int i = 0; i < nums.Length; i++)
+        { // find max
+            if (nums[i] > max)
+            {
+                max = nums[i];
+            }
+        }
+
+        int[] outputArr = new int[findNums.Length];
+        for(int i = 0; i < outputArr.Length; i++)
+        {
+            outputArr[i] = -1;
+        }
+
+        for (int i = 0; i < findNums.Length; i++)
+        {
+            if (findNums[i] >= max)
+            {
+                continue;
+            }
+            else
+            {
+                bool found = false;
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[j] == findNums[i])
+                    {
+                        found = true;
+                    }                
+                    if (found)
+                    {
+                        if (nums[j] > findNums[i])
+                        {
+                            outputArr[i] = nums[j];
+                            break;
+                        }
+                    }
+                }
+            }        
+        }
+        return outputArr;
+    }
+}
