052db0a53bc85e85a2d2e78e10f36468db343fc6
Matt <matt.paauw@gmail.com>
2017-08-18 14:02:11 -0700

    Added algorithm #238.

---
+++ C#/238-product-of-array-except-self.cs
@@ -0,0 +1,20 @@
+public class Solution
+{
+    public int[] ProductExceptSelf(int[] nums) // solution poached from discussion
+    {
+        int n = nums.Length;
+        int[] res = new int[n];
+        res[0] = 1;
+        for (int i = 1; i < n; i++)
+        {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            res[i] *= right;
+            right *= nums[i];
+        }
+        return res;
+    }
+}
