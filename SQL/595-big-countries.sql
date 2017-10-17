88e8b02db7aceb9c38a9a9ccb0b4f3f6319c1406
Matt <matt.paauw@gmail.com>
2017-10-17 16:59:56 -0700

    Modified algorithm 595, added more efficient solution and fixed typo in previous submission.

--- SQL/595-big-countries.sql
+++ SQL/595-big-countries.sql
@@ -1,1 +1,8 @@
-SELECT name, population, area FROM World WHERE area > 3000000 OR population  > 25000000SELECT name, population, area FROM World WHERE area > 3000000 OR population  > 25000000
+# Standard obvious submission:
+
+SELECT name, population, area FROM World WHERE area > 3000000 OR population > 25000000
+
+# Faster submission using UNION:
+SELECT name, population, area FROM World WHERE area > 3000000
+UNION
+SELECT name, population, area FROM World WHERE population > 25000000
