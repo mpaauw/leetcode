be056862840d7b941f64e25671373bde58c1ee52
mpaauw <matt.paauw@gmail.com>
2018-07-11 08:39:58 -0700

    Cleaned up 807.

--- C#/807-max-increase-to-keep-city-skyline.cs
+++ C#/807-max-increase-to-keep-city-skyline.cs
@@ -2,11 +2,6 @@ public class Solution
 {
     public int MaxIncreaseKeepingSkyline(int[][] grid)
     {
-
-        // for each cell:
-        //  - get vertical / horizontal max
-        //  - new value is the lesser of the vert / hori maxes
-        //      - if both are equal, new value is resultant
         int numRows = (grid.Length > 0) ? grid.Length : throw new ArgumentException();
         int numCols = (grid[0].Length > 0) ? grid[0].Length : throw new ArgumentException();
         int totalSum = 0;
@@ -15,7 +10,6 @@ public class Solution
         {
             for (int col = 0; col < numCols; col++)
             {
-                // get vert max:
                 int vertMax = 0;
                 for (int i = 0; i < numRows; i++)
                 {
@@ -25,7 +19,6 @@ public class Solution
                     }
                 }
 
-                // get hori max:
                 int horiMax = 0;
                 for (int i = 0; i < numCols; i++)
                 {
